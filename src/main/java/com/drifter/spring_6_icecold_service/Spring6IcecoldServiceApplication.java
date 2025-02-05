4ffa18e86f2b068d3002a664b681740bd6f9db18
DonMB77 <purposefuldrifter@gmail.com>
2025-02-05 16:10:53 +0100

    initial init

---
+++ src/main/java/com/drifter/spring_6_icecold_service/Spring6IcecoldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_6_icecold_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6IcecoldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6IcecoldServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drifter/spring_6_icecold_service/Spring6IcecoldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_6_icecold_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6IcecoldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
